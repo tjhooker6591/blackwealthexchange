4a1b07940801a4add3d197443aee7ce4e2ec65da
tjhooker6591 <tjameshooker@gmail.com>
2025-03-20 20:54:01 -0700

    updated the signup.tsx file for db changes

--- src/pages/api/business/signup.js
+++ src/pages/api/business/signup.js
@@ -1,6 +1,6 @@
 // pages/api/business/signup.js
 
-import dbConnect from "../../lib/mongodb"; // MongoDB connection utility
+import dbConnect from "../../../lib/mongodb"; // MongoDB connection utility
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
