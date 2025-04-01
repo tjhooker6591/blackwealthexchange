223806d434155c166efe7f17d4bb48275643f879
tjhooker6591 <tjameshooker@gmail.com>
2025-04-01 15:28:32 -0700

    add the favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,22 @@
-// pages/_app.tsx
-
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/footer";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      {/* Global <head> settings */}
+      <Head>
+        <link rel="icon" href="/images/favicon.ico" />
+        <title>Black Wealth Exchange</title>
+        <meta
+          name="description"
+          content="Empowering Black-owned businesses and wealth-building."
+        />
+      </Head>
+
       <NavBar />
       <main className="min-h-screen">
         <Component {...pageProps} />
