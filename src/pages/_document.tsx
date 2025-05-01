aa5dfdd67dcabe239424eee57735cbe79ad94ebd
tjhooker6591 <tjameshooker@gmail.com>
2025-05-01 10:20:03 -0700

    favicon updates

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,14 @@ export default function Document() {
       <Head>
         {/* optional: sets Android address‑bar color */}
         <meta name="theme-color" content="#000000" />
+
+        {/* Favicons */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="preload" as="image" href="/favicon.ico" />
       </Head>
       <body className="bg-black antialiased overflow-x-hidden">
         <Main />
